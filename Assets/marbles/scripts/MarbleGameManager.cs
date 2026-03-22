d2fc8b8639fd9904d1b58d033ebdda6edee7935d
Suhani <shadowknight0316@gmail.com>
2026-03-22 14:41:35 +0530

    made changes to marble scene logic

--- Assets/marbles/scripts/MarbleGameManager.cs
+++ Assets/marbles/scripts/MarbleGameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MarbleGameManager : MonoBehaviour
@@ -7,37 +8,102 @@ public class MarbleGameManager : MonoBehaviour
     public int correctGuesses = 0;
     public int wrongGuesses = 0;
 
+    [Header("Prefabs")]
+    public GameObject marblePrefab;
+    public GameObject fakePrefab;
+
+    [Header("Box Settings")]
+    public Transform spawnArea;
+    public Vector3 spawnSize = new Vector3(2f, 2f, 2f);
+    public Transform lid;
+
+    private List<GameObject> spawnedObjects = new List<GameObject>();
+
+    void Start()
+    {
+        EnemyHideSquares();
+    }
+
     public void EnemyHideSquares()
     {
-        hiddenSquares = Random.Range(1, 6);
-        Debug.Log("Enemy has hidden squares (1–5).");
+        ClearOld();
+
+        int totalObjects = Random.Range(1, 6); 
+
+        for (int i = 0; i < totalObjects - 1; i++)
+        {
+            Spawn(marblePrefab);
+        }
+
+        
+        Spawn(fakePrefab);
+
+        hiddenSquares = totalObjects - 1;
+
+        CloseLid();
+
+        Debug.Log("New round started.");
+    }
+
+    void Spawn(GameObject prefab)
+    {
+        Vector3 pos = spawnArea.position + new Vector3(
+            Random.Range(-spawnSize.x / 2, spawnSize.x / 2),
+            Random.Range(-spawnSize.y / 2, spawnSize.y / 2),
+            Random.Range(-spawnSize.z / 2, spawnSize.z / 2)
+        );
+
+        GameObject obj = Instantiate(prefab, pos, Random.rotation);
+        spawnedObjects.Add(obj);
+    }
+
+    void ClearOld()
+    {
+        foreach (GameObject obj in spawnedObjects)
+        {
+            Destroy(obj);
+        }
+        spawnedObjects.Clear();
     }
 
     public void PlayerGuess(int guess)
     {
         Debug.Log("Player guessed: " + guess);
 
+        OpenLid(); 
+
         if (guess == hiddenSquares)
         {
             correctGuesses++;
-            Debug.Log("✅ Correct! Total correct guesses: " + correctGuesses);
+            Debug.Log(" Correct! Total correct guesses: " + correctGuesses);
         }
         else
         {
             wrongGuesses++;
-            Debug.Log("❌ Wrong guess.");
+            Debug.Log("Wrong! (You counted the fake, didn’t you?)");
         }
 
         CheckWin();
 
-        EnemyHideSquares(); // start next round automatically
-    }
+        Invoke(nameof(EnemyHideSquares), 2f);  }
 
     void CheckWin()
     {
         if (correctGuesses >= 3)
         {
-            Debug.Log("🏆 Player wins! 3 correct guesses achieved.");
+            Debug.Log(" Player wins! 3 correct guesses achieved.");
         }
+    }
+
+    void OpenLid()
+    {
+        if (lid != null)
+            lid.localRotation = Quaternion.Euler(-110f, 0, 0);
+    }
+
+    void CloseLid()
+    {
+        if (lid != null)
+            lid.localRotation = Quaternion.Euler(0, 0, 0);
     }
 }
--- Assets/redlight/script/Trigger.cs
+++ Assets/redlight/script/Trigger.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Player entered the trigger!");
-            // You can add additional logic here, such as activating a door, starting a cutscene, etc.
         }
     }
     
@@ -17,8 +15,7 @@ public class Trigger : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+      void Update()
     {
         
     }
